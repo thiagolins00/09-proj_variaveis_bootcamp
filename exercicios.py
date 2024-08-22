387fccfacc122ad16cd5c157876591ac9e853e9e
Thiago Lins <thiagolins00@gmail.com>
2024-08-21 23:24:30 -0300

    Segundo commit

---
+++ exercicios.py
@@ -0,0 +1,14 @@
+'''
+nome_aluno = "Thiago@gmail.com"
+
+print(nome_aluno.split("@"))
+
+'''
+
+valor_1 = int(input("Digite o valor 1: " ))
+valor_2 = int(input("Digite o valor 2: " ))
+
+
+resultado = (valor_1 + valor_2)
+
+print (f"Resultado: ", resultado)
