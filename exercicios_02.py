bcdbacc4e057249b69b56bc5b545ad63e2df25f5
Thiago Lins <thiagolins00@gmail.com>
2024-08-22 21:53:04 -0300

    Adicionando novo arquivo

---
+++ exercicios_02.py
@@ -0,0 +1,27 @@
+# Números de Ponto Flutuante (float)
+
+# 06 - Escreva um programa que receba dois números flutuantes e realize sua adição.
+
+
+
+
+# 07 - Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+
+
+
+
+# 08 - Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+
+
+
+
+# 09 - Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+
+
+
+
+# 10 - Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+
+
+
+
