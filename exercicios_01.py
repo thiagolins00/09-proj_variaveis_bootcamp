4932299d82e097addf339d4b095e6ff49f6e812d
Thiago Lins <thiagolins00@gmail.com>
2024-08-22 21:44:00 -0300

    Exercicio 01 concluído

---
+++ exercicios_01.py
@@ -0,0 +1,53 @@
+# Inteiros (int)
+
+
+# 1 - Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+'''
+valor_01 = int(input("Digite o valor: "))
+
+valor_02 = int(input("Digite o valor: "))
+
+resultado_final_soma = (valor_01 + valor_02)
+
+print(f"O valor total foi",resultado_final_soma, "parabéns Thiago." )'''
+
+# 2 - Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+'''
+CONSTRANTS_FIXO = 5
+
+valor_01 = int(input("Digite o valor: "))
+
+resultado_final_soma = int((valor_01 / CONSTRANTS_FIXO))
+
+print(f"O valor total foi",resultado_final_soma,"parabéns Thiago." )'''
+
+# 3 - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+'''
+valor_01 = int(input("Digite o valor: "))
+
+valor_02 = int(input("Digite o valor: "))
+
+resultado_final_soma = int((valor_01 * valor_02))
+
+print(f"O valor total foi",resultado_final_soma,"parabéns Thiago." )'''
+
+# 4 - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+'''
+valor_01 = int(input("Digite o valor: "))
+
+valor_02 = int(input("Digite o valor: "))
+
+resultado_final_soma = int((valor_01 // valor_01))
+
+print(f"O valor total foi",resultado_final_soma,"parabéns Thiago." )'''
+
+# 5 - Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+
+num = int(input("Digite um número: "))
+#num = 6  # Exemplo de entrada
+resultado_quadrado = num ** 2
+print("O quadrado do número é:", resultado_quadrado)
